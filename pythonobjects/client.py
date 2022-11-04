c38ffeeb2a8a080ccfecf5119e97e92137935e57
iririb <eshamibrahim94@gmail.com>
2022-11-04 17:45:38 +0800

    Transmitting object using pickle without casting

--- pythonobjects/client.py
+++ pythonobjects/client.py
@@ -1,8 +1,9 @@
 import socket
 from product import Product
+import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((socket.gethostname(),4567))
+    s.connect((socket.gethostname(), 4567))
 
     while True:
         msg = s.recv(1024)
@@ -10,5 +11,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         if not msg:
             print('Nothing received from server anymore')
             break
-        print(msg.decode('utf-8'))
+        
         print(type(msg))
+        print(msg)
+        unpickled_msg = pickle.loads(msg)
+        print(type(unpickled_msg))
+        print(unpickled_msg)
--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -1,10 +1,19 @@
 import socket
 from product import Product
+# serializing and deserializing a python object
+import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((socket.gethostname(), 4567))
+    
     python_dictionary = {'a': 1, 'b': 2}
+    pickled_dictionary = pickle.dumps(python_dictionary)
+
     custom_object = Product('P012', 'Torch', 11)
+    pickled_object = pickle.dumps(custom_object)
+
+    print(type(pickled_dictionary))
+    print(type(pickled_object))
 
     s.listen(5)
     print('Server is up')
@@ -13,5 +22,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(client)
     print(address)
 
-    client.send(bytes(str(python_dictionary),'utf-8'))
-    client.send(bytes(str(custom_object),'utf-8'))
+    client.send(pickled_dictionary)
+    # client.send(pickled_object)
